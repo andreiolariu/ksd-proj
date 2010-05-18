0b056b7ab4331176c0ba54af3f8eccb665df99fa
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2010-05-18 20:33:36 +0300

    update.sh

--- update.py
+++ update.py
@@ -1,6 +1,10 @@
 from libs.indexmanager import FolderManager
+from index import IndexFiles
 
 if __name__ == '__main__':
   manager = FolderManager()
+  indexer = IndexFiles()
   for followed in manager.get_followed():
-    # index(followed)
+    print 'Updating folder %s...' % followed
+    indexer.index_folder(followed)
+  print 'Updated.'
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python update.py
