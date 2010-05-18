32a1f7a6a2fb237ed452c2540c836af0529a0a52
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2010-05-18 21:51:44 +0300

    improved monitor

--- monitor.py
+++ monitor.py
@@ -6,6 +6,7 @@ import Queue
 from index import IndexFiles
 
 from libs.indexmanager import FolderManager
+from libs.lrucache import LRUCache
 
 def run(command):
   try:
@@ -18,22 +19,35 @@ def run(command):
 class HandleEvents(pyinotify.ProcessEvent):
     def __init__(self, queue):
       pyinotify.ProcessEvent.__init__(self)
+      self.lrucache = LRUCache(1000)
       self.queue = queue
+
+    def recent(self, pathname):
+      # Was this file just modified a few seconds ago?
+      last_detection = 0
+      if pathname in self.lrucache:
+        last_detection = self.lrucache[pathname]
+      self.lrucache[pathname] = time.time()
+      return time.time() - last_detection < 2
+
     def process_IN_CREATE(self, event):
-        print "Creating:", event.pathname
-        self.queue.put(('+', event.pathname))
-        #self.indexer.index(None, {'+': event.pathname})
+      if self.recent(event.pathname):
+        return
+      print "Creating:", event.pathname
+      self.queue.put(('+', event.pathname))
 
     def process_IN_DELETE(self, event):
-        print "Removing:", event.pathname
-        self.queue.put(('-', event.pathname))
-        #self.indexer.index(None, {'-': event.pathname})
+      if self.recent(event.pathname):
+        return
+      print "Removing:", event.pathname
+      self.queue.put(('-', event.pathname))
 
     def process_IN_MODIFY(self, event):
-        print "Modifying:", event.pathname
-        self.queue.put(('-', event.pathname))
-        self.queue.put(('+', event.pathname))
-        #self.indexer.index(None, {'-': event.pathname, '+': event.pathname})
+      if self.recent(event.pathname):
+        return
+      print "Modifying:", event.pathname
+      self.queue.put(('-', event.pathname))
+      self.queue.put(('+', event.pathname))
 
 class Monitor(object):
 
@@ -46,17 +60,18 @@ class Monitor(object):
 
     self.queue = Queue.Queue()
     handler = HandleEvents(self.queue)
+    self.manager = FolderManager()
     self.notifier = pyinotify.ThreadedNotifier(self.wm, handler)
     self.notifier.start()
-    self.manager = FolderManager()
 
   def run(self):
     indexer = IndexFiles()
     while self.runsignal:
       for folder in self.manager.get_followed():
-        mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
-        self.wm.add_watch(folder, mask, rec=True)
-        print 'Monitoring folder %s...' % folder
+        if not self.wm.get_wd(folder):
+          mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY #| pyinotify.IN_MOVED_TO
+          self.wm.add_watch(folder, mask, rec=True)
+          print 'Monitoring folder %s...' % folder
       try:
         data = self.queue.get(False)
       except:
