6d2ff875a8d8f76f19a1ad0d7162942629b5fb8e
ubervuadi <ditzone@gmail.com>
2010-05-21 17:39:38 +0300

    Setup update.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@
 # TODO(wget & unrar)
 # http://mirrors.hostingromania.ro/apache.org/lucene/pylucene/
 
+wget http://peak.telecommunity.com/dist/ez_setup.py
+sudo python ez_setup.py
 sudo easy_install pyinotify
